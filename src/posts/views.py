406b9aa6647f223bc0f1cba6a8a28660854180a1
Ismael Bernal <ismaelbernal83@gmail.com>
2017-11-20 23:34:02 +0100

    20th November 2017

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def posts_create(request):
+    return HttpResponse("<h1>CREATE</h1")
+
+def posts_detail(request):
+    return HttpResponse("<h1>RETRIEVE (ONE)</h1")
+
+def posts_list(request):
+    return HttpResponse("<h1>RETRIEVE (LIST)</h1")
+
+def posts_update(request):
+    return HttpResponse("<h1>UPDATE</h1")
+
+def posts_delete(request):
+    return HttpResponse("<h1>DELETE</h1")

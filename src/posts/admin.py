ba9cea0e96dab9037f7f252eaca92533c65cb6c6
Ismael Bernal <ismaelbernal83@gmail.com>
2017-11-20 23:33:50 +0100

    20th November 2017

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -5,7 +5,21 @@ from .models import Post
 
 # More info here: https://docs.djangoproject.com/en/1.11/intro/tutorial07/
 class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "content", "updated", "timestamp"]
+    list_display_links = ["title"]
+    list_editable = ["content"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
     class Meta:
         model = Post
 
 admin.site.register(Post, PostModelAdmin)
+
+# Admin is very good to model the application because it
+# easily allow us to test the CRUD concept:#
+#   CRUD:   Create* -- POST
+#           Retrieve -- GET (Examples: List / Search)
+#           Update* -- PUT/PATCH
+#           Delete* -- DELETE
+#
+#  * They will require permnissons

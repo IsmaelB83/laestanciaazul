3074720acf6a904c8456241880c182f779fd26c5
Ismael Bernal <ismaelbernal83@gmail.com>
2017-11-21 23:18:02 +0100

    21st November 2017

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,26 +1,58 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def posts_create(request):
-    return HttpResponse("<h1>CREATE</h1")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.save()
+        messages.success(request, "Successfully created", extra_tags="some-tag success")
+        return HttpResponseRedirect(post.get_absolute_url())
 
-def posts_detail(request):
-    return HttpResponse("<h1>RETRIEVE (ONE)</h1")
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
+
+
+def posts_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    context = {
+        "title": post.title,
+        "post": post,
+    }
+    return render(request, "post_detail.html", context)
+
 
 def posts_list(request):
-    if request.user.is_authenticated():
-        context = {
-            "title": "My User List",
-        }
-    else:
-        context = {
-            "title": "List",
-        }
+    posts = Post.objects.all()
+    context = {
+        "title": "List",
+        "posts": posts
+    }
     return render(request, "index.html", context)
 
-def posts_update(request):
-    return HttpResponse("<h1>UPDATE</h1")
+
+def posts_update(request, id):
+    post = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.save()
+        messages.success(request, "<a href=''>Item saved</a>", extra_tags="html_seguro")
+        return HttpResponseRedirect(post.get_absolute_url())
+
+    context = {
+        "title": post.title,
+        "post": post,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 def posts_delete(request):
     return HttpResponse("<h1>DELETE</h1")

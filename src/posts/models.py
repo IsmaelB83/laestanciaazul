2735b5a3343dcfe1291a47ab28d9ed58c77e5a0f
Ismael Bernal <ismaelbernal83@gmail.com>
2017-11-21 23:18:02 +0100

    21st November 2017

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 # MVC Model View Controller
@@ -15,3 +16,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse("posts:detail", kwargs={"id": self.id})
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^create/$',views.posts_create, name='post_create'),
-    url(r'^detail/$', views.posts_detail, name='post_detail'),
-    url(r'^list/$', views.posts_list, name='post_list'),
-    url(r'^update/$', views.posts_update, name='post_update'),
-    url(r'^delete/$', views.posts_delete, name='post_delete'),
+    url(r'^create/$', views.posts_create),
+    url(r'^(?P<id>\d+)/$', views.posts_detail, name='detail'),
+    url(r'^list/$', views.posts_list),
+    url(r'^(?P<id>\d+)/edit/$', views.posts_update),
+    url(r'^delete/$', views.posts_delete),
 ]
 

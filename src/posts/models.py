082cfb6e300e34a51c4f3c516a59373a7a540ef0
Ismael Bernal <ismaelbernal83@gmail.com>
2017-11-24 17:17:25 +0100

    24th November 2017

--- src/posts/models.py
+++ src/posts/models.py
@@ -3,12 +3,24 @@ from django.core.urlresolvers import reverse
 
 # Create your models here.
 # MVC Model View Controller
+def upload_location(instance, filename):
+    return "%s/%i/%s" % (instance.author, instance.id, filename)
 
 class Post(models.Model):
-    title = models.CharField(max_length=120)
-    content = models.TextField()
-    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    title = models.CharField(null=False, blank=False, max_length=120)
+    content = models.TextField(null=False, blank=False)
+    author = models.CharField(null=False, max_length=20)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    caption = models.URLField()
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
+    image = models.ImageField(
+        upload_to=upload_location,
+        null=True,
+        blank=True,
+        height_field="height_field",
+        width_field="width_field")
 
     def __unicode__(self):
         return self.title
@@ -18,3 +30,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("posts:detail", kwargs={"id": self.id})
+
+    def get_lists_url(self):
+        return reverse("posts:list")
+
+    class Meta:
+        ordering = ["-timestamp", "-updated"]

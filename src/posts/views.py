ac5db821f661049519c4fbe435958c4f2b46a29f
Ismael Bernal <ismaelbernal83@gmail.com>
2017-12-14 07:30:51 +0100

    24th November 2017

--- src/posts/views.py
+++ src/posts/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from .models import Post
+from .models import Post, Category, PostCategory
 from .forms import PostForm
 
 # Create your views here.
 def index(request):
     posts_all = Post.objects.all()
+    post_categories = PostCategory.objects.all()
     paginator = Paginator(posts_all, 3)
     page_request_var = 'page'
     page = request.GET.get('page')
@@ -18,67 +19,78 @@ def index(request):
         posts_page = paginator.page(1)
     except EmptyPage:
         posts_page = paginator.page(paginator.num_pages)
-    context = {
-        "title": "List of Posts",
-        "page_obj": posts_page,
-        "page_request_var": page_request_var
+    context = {'title': 'List of Posts', 'posts': posts_page, 'post_categories': post_categories, 'page_request': page_request_var,
     }
-    return render(request, "index.html", context)
+    return render(request, 'index.html', context)
 
 
 def contact(request):
-    return render(request, "contact.html")
+    return render(request, 'contact.html')
 
 
 def archive(request):
-    return render(request, "archive.html")
-
-
-def posts_detail(request, id):
-    post = get_object_or_404(Post, id=id)
-    context = {
-        "title": post.title,
-        "post": post,
+    posts_all = Post.objects.all()
+    post_categories = PostCategory.objects.all()
+    paginator = Paginator(posts_all, 3)
+    page_request_var = 'page'
+    page = request.GET.get('page')
+    try:
+        posts_page = paginator.page(page)
+    except PageNotAnInteger:
+        posts_page = paginator.page(1)
+    except EmptyPage:
+        posts_page = paginator.page(paginator.num_pages)
+    context = {'title': 'List of Posts', 'posts': posts_page, 'post_categories': post_categories, 'page_request': page_request_var
     }
-    return render(request, "post.html", context)
+    return render(request, 'archive.html', context)
 
 
 def category(request, id):
-    categorias = {
-        'all': 'TODO',
-        'prog': 'PROGRAMACIÓN',
-        'rpi': 'RASPBERY PI',
-        'linux': 'LINUX',
-        'sap': 'SAP',
-        'other': 'OTROS'
+    category = Category.objects.get(id=id)
+    post_categories = PostCategory.objects.filter(category=category)
+    if id != 'all':
+        posts_all = Post.objects.filter(postcategory__in=post_categories)
+    else:
+        posts_all = Post.objects.all()
+    post_categories = PostCategory.objects.filter(post__in=posts_all)
+    paginator = Paginator(posts_all, 3)
+    page_request_var = 'page'
+    page = request.GET.get('page')
+    try:
+        posts_page = paginator.page(page)
+    except PageNotAnInteger:
+        posts_page = paginator.page(1)
+    except EmptyPage:
+        posts_page = paginator.page(paginator.num_pages)
+    context = {'title': 'Category', 'category': category, 'posts': posts_page, 'post_categories': post_categories, 'page_request': page_request_var
     }
-    categoria = categorias[id]
-    context = {
-        'title': "Category",
-        'category': categoria
-    }
-    return render(request, "category.html", context)
+    return render(request, 'category.html', context)
+
+
+def post(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    post_categories = PostCategory.objects.filter(post=post)
+    context = {'title': post.title, 'post': post, 'post_categories': post_categories, }
+    return render(request, 'post.html', context)
 
 
 def posts_create(request, category):
-
     if request.user.is_authenticated:
         form = PostForm(request.POST or None, request.FILES or None)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            messages.success(request, "Successfully created")
-            return redirect("blog:list")
+            messages.success(request, 'Successfully created')
+            return redirect('blog:list')
 
-        context = {
-            "form": form,
-            "title": "Create new post"
+        context = {'form': form, 'title': 'Create new post'
         }
-        return render(request, "post_form.html", context)
+        return render(request, 'post_form.html', context)
     else:
-        messages.error(request, "Not authenticated")
-        return redirect("blog:list")
+        messages.error(request, 'Not authenticated')
+        return redirect('blog:list')
 
 def posts_list(request):
     posts_all = Post.objects.all()
@@ -91,12 +103,9 @@ def posts_list(request):
         posts_page = paginator.page(1)
     except EmptyPage:
         posts_page = paginator.page(paginator.num_pages)
-    context = {
-        "title": "List of Posts",
-        "posts": posts_page,
-        "page_request_var": page_request_var
+    context = {'title': 'List of Posts', 'posts': posts_page, 'page_request_var': page_request_var
     }
-    return render(request, "post_list.html", context)
+    return render(request, 'post_list.html', context)
 
 def posts_update(request, id):
     if request.user.is_authenticated:
@@ -105,24 +114,21 @@ def posts_update(request, id):
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
-            messages.success(request, "<a href=''>Item saved</a>", extra_tags="html_seguro")
-            return redirect("blog:list")
+            messages.success(request, '<a href="">Item saved</a>', extra_tags='html_seguro')
+            return redirect('blog:list')
 
-        context = {
-            "title": "Edit post: " + post.title,
-            "post": post,
-            "form": form,
+        context = {'title': 'Edit post: ' + post.title, 'post': post, 'form': form,
         }
-        return render(request, "post_form.html", context)
+        return render(request, 'post_form.html', context)
     else:
-        messages.error(request, "Not authenticated")
-        return redirect("blog:list")
+        messages.error(request, 'Not authenticated')
+        return redirect('blog:list')
 
 def posts_delete(request, id):
     if request.user.is_authenticated:
         post = get_object_or_404(Post, id=id)
         post.delete()
-        messages.success(request, "Successfully deleted")
+        messages.success(request, 'Successfully deleted')
     else:
-        messages.error(request, "Not authenticated")
-    return redirect("blog:list")
+        messages.error(request, 'Not authenticated')
+    return redirect('blog:list')

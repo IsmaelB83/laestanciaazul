e786ae732b27d43e2363452a307fba929a8c43a8
Ismael Bernal <ismaelbernal83@gmail.com>
2017-12-20 00:35:21 +0100

    24th November 2017

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -4,22 +4,22 @@ from .models import Post, PostComment, Category
 
 CATEGORY_CHOICES = [[c.id, c.category] for c in Category.objects.all()]
 
+
 class PostForm(forms.ModelForm):
-    categorias_post = forms.MultipleChoiceField(choices=CATEGORY_CHOICES, required=False, widget=forms.CheckboxSelectMultiple, )
-    title = forms.CharField(label="Title", required=True, max_length=30, widget=forms.TextInput(attrs={'placeholder': 'title', 'autocomplete': 'off'}))
+    postcategory = forms.MultipleChoiceField(choices=CATEGORY_CHOICES, required=True, widget=forms.CheckboxSelectMultiple, )
+    status = forms.ChoiceField(choices=Post.STATUSES, required=True, widget=forms.RadioSelect, )
 
     class Meta:
         model = Post
-        fields = ["categorias_post", "title", ]
+        fields = ('title', 'postcategory', 'status', 'published_date', 'image', 'content')
 
 
 class PostCommentForm(forms.ModelForm):
     class Meta:
         model = PostComment
-        fields = ["anonymous_name", "anonymous_email", "comment",
-        ]
+        fields = ['anonymous_name', 'anonymous_email', 'comment', ]
 
 
 class LoginForm():
-    user = forms.CharField(label="Username", required=True, max_length=20, widget=forms.TextInput(attrs={'placeholder': 'username', 'autocomplete': 'off'}))
-    password = forms.CharField(label="Password", required=True, max_length=100, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'autocomplete': 'off'}))
+    user = forms.CharField(label='Username', required=True, max_length=20, widget=forms.TextInput(attrs={'placeholder': 'username', 'autocomplete': 'off'}))
+    password = forms.CharField(label='Password', required=True, max_length=100, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'autocomplete': 'off'}))

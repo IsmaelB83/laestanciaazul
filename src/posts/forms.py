f221ea0286e38093b27f9c57139602db6677849b
Ismael Bernal <ismaelbernal83@gmail.com>
2017-12-21 07:39:53 +0100

    Diciembre 21 - Post create

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from multiupload.fields import MultiFileField
 
 from .models import Post, PostComment, Category
 
@@ -8,10 +9,11 @@ CATEGORY_CHOICES = [[c.id, c.category] for c in Category.objects.all()]
 class PostForm(forms.ModelForm):
     postcategory = forms.MultipleChoiceField(choices=CATEGORY_CHOICES, required=True, widget=forms.CheckboxSelectMultiple, )
     status = forms.ChoiceField(choices=Post.STATUSES, required=True, widget=forms.RadioSelect, )
+    postimage = MultiFileField(min_num=0, max_num=4, max_file_size=1024 * 1024)
 
     class Meta:
         model = Post
-        fields = ('title', 'postcategory', 'status', 'published_date', 'image', 'content')
+        fields = ('title', 'postcategory', 'status', 'published_date', 'image', 'content', 'postimage')
 
 
 class PostCommentForm(forms.ModelForm):

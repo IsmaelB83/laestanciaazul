ff48acb50eceeac3e28509f8aa474471483e4938
Ismael Bernal <ismaelbernal83@gmail.com>
2018-01-09 07:22:35 +0100

    03 January

--- src/post/templatetags/post_extras.py
+++ src/post/templatetags/post_extras.py
@@ -1,14 +1,17 @@
 from django import template
 import calendar
+import locale
 
 register = template.Library()
 
 @register.filter
 def month_name(month_number):
+    locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
     return calendar.month_name[month_number]
 
 @register.filter
 def month_inverse(month_number):
+    locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
     return calendar.month_name[14-month_number]
 
 @register.filter
--- src/web/settings.py
+++ src/web/settings.py
@@ -123,17 +123,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'es-ES'
 TIME_ZONE = 'Europe/Madrid'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
